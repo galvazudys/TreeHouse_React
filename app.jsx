341b550515ca47ebf40eb91578132795383d1b39
galvazudys <golfas705@yahoo.com>
2017-04-11 10:50:08 +0100

    new function

--- app.jsx
+++ app.jsx
@@ -1,1 +1,20 @@
-ReactDOM.render
+function Application() {
+    return (
+        <div>
+            <h1>
+                <strong>Hello!!!</strong>
+                <p>Vivamus suscipit tortor eget felis porttitor volutpat. Lorem ipsum dolor sit
+                    amet, consectetur adipiscing elit. Pellentesque in ipsum id orci porta dapibus.
+                    Quisque velit nisi, pretium ut lacinia in, elementum id enim. Lorem ipsum dolor
+                    sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit. Curabitur non nulla sit amet nisl tempus convallis quis ac
+                    lectus. Curabitur aliquet quam id dui posuere blandit. Pellentesque in ipsum id
+                    orci porta dapibus. Praesent sapien massa, convallis a pellentesque nec, egestas
+                    non nisi.</p>
+            </h1>
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <Application/>, document.querySelector('#container'));
