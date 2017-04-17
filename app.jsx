8a67aa2ebdb6bb5908bdf43d4b62704a42e0e098
galvazudys <golfas705@yahoo.com>
2017-04-17 09:30:14 +0100

    Added form,to add players

--- app.jsx
+++ app.jsx
@@ -18,45 +18,121 @@ var PLAYERS = [
     }
 ];
 
+var nextId = 5;
+
+var AddPlayerForm = React.createClass({
+    propTypes: {
+        onAdd: React.PropTypes.func.isRequired
+    },
+
+    onNameChange: function (e) {
+        this.setState({name: e.target.value});
+    },
+
+    onSubmit: function (e) {
+        e.preventDefault();
+
+        this
+            .props
+            .onAdd(this.state.name);
+        this.setState({name: ""});
+    },
+
+    getInitialState: function () {
+        return {name: ""};
+    },
+
+    render: function () {
+        return (
+            <div className="add-player-form">
+                <form onSubmit={this.onSubmit}>
+                    <input type="text" value={this.state.name} onChange={this.onNameChange}/>
+                    <input type="submit" value="Add Player"/>
+                </form>
+            </div>
+        );
+    }
+});
+
+function Stats(props) {
+    var totalPlayers = props.players.length;
+    var totalPoints = props
+        .players
+        .reduce(function (total, player) {
+            return total + player.score;
+        }, 0);
+
+    return (
+        <table classsName="stats">
+            <tbody>
+                <tr>
+                    <td>Players:</td>
+                    <td>{totalPlayers}</td>
+                </tr>
+                <tr>
+                    <td>Total Points:</td>
+                    <td>{totalPoints}</td>
+                </tr>
+            </tbody>
+        </table>
+    );
+}
+
+Stats.prototype = {
+    players: React.PropTypes.array.isRequired
+};
+
 function Header(props) {
     return (
         <div className="header">
+            <Stats players={props.players}/>
             <h1>{props.title}</h1>
         </div>
     );
 }
 
 Header.propTypes = {
-    title: React.PropTypes.string.isRequired
+    title: React.PropTypes.string.isRequired,
+    players: React.PropTypes.array.isRequired
 };
 
 function Counter(props) {
     return (
         <div className="counter">
-            <button className="counter-action decrement">
+            <button
+                className="counter-action decrement"
+                onClick={function () {
+                props.onChange(-1);
+            }}>
                 -
             </button>
             <div className="counter-score">
                 {props.score}
             </div>
-            <button className="counter-action increment">
+            <button
+                className="counter-action increment"
+                onClick={function () {
+                props.onChange(+ 1);
+            }}>
                 +
             </button>
         </div>
     );
 }
 Counter.propTypes = {
-    score: React.PropTypes.number.isRequired
+    score: React.PropTypes.number.isRequired,
+    onChange: React.PropTypes.func.isRequired
 }
 
 function Player(props) {
     return (
         <div className="player">
             <div className="player-name">
+                <a className="remove-player" onClick={props.onRemove}>X</a>
                 {props.name}
             </div>
             <div className="player-score">
-                <Counter score={props.score}/>
+                <Counter score={props.score} onChange={props.onScoreChange}/>
             </div>
         </div>
     );
@@ -64,13 +140,15 @@ function Player(props) {
 
 Player.propTypes = {
     name: React.PropTypes.string.isRequired,
-    score: React.PropTypes.number.isRequired
+    score: React.PropTypes.number.isRequired,
+    onScoreChange: React.PropTypes.func.isRequired,
+    onRemove:React.PropTypes.func.isRequired
 };
 
 var Application = React.createClass({
     propTypes: {
         title: React.PropTypes.string,
-        players: React
+        initialPlayers: React
             .PropTypes
             .arrayOf(React.PropTypes.shape({name: React.PropTypes.string.isRequired, score: React.PropTypes.number.isRequired, id: React.PropTypes.number.isRequired}))
             .isRequired
@@ -81,22 +159,53 @@ var Application = React.createClass({
         return {title: "Scoreborad"}
     },
 
+    getInitialState: function () {
+        return {players: this.props.initialPlayers}
+    },
+
+    onScoreChange: function (index, delta) {
+        this.state.players[index].score += delta;
+        this.setState(this.state);
+    },
+
+    onPlayerAdd: function (name) {
+        this
+            .state
+            .players
+            .push({name: name, score: 0, id: nextId});
+        this.setState(this.state);
+        nextId += 1;
+    },
+
+    onRemovePlayer:function(index){
+        this.state.players.splice(index,1);
+        this.setState(this.state);
+    },
+
     render: function () {
         return (
             <div className="scoreboard">
-                <Header title={this.props.title}/>
+                <Header title={this.props.title} players={this.state.players}/>
                 <div className="players">
                     {this
-                        .props
+                        .state
                         .players
-                        .map(function (player) {
-                            return <Player name={player.name} score={player.score} key={player.id}/>
-                        })}
+                        .map(function (player, index) {
+                            return (<Player
+                                onScoreChange={function (delta) {
+                                this.onScoreChange(index, delta)
+                            }.bind(this)}
+                                onRemove={function(){this.onRemovePlayer(index)}.bind(this)}
+                                name={player.name}
+                                score={player.score}
+                                key={player.id}/>);
+                        }.bind(this))}
                 </div>
+                <AddPlayerForm onAdd={this.onPlayerAdd}/>
             </div>
         );
     }
 });
 
 ReactDOM.render(
-    <Application players={PLAYERS}/>, document.querySelector('#container'));
+    <Application initialPlayers={PLAYERS}/>, document.querySelector('#container'));
